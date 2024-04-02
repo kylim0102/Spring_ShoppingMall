92ea302512590b3b5150999773fe63867e1cdd32
kylim0102 <ghckddl0102@naver.com>
2024-04-02 17:31:52 +0900

    20240402

--- src/main/java/lhc/portfolio_test/controller/MyPageController.java
+++ src/main/java/lhc/portfolio_test/controller/MyPageController.java
@@ -1,22 +1,22 @@
 package lhc.portfolio_test.controller;
 
-import lhc.portfolio_test.dto.CartDTO;
 import lhc.portfolio_test.dto.CustomUserDetails;
 import lhc.portfolio_test.dto.MyPageDTO;
-import lhc.portfolio_test.repository.ProductRepository;
+import lhc.portfolio_test.service.CartService;
 import lhc.portfolio_test.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyPageController {
 
     @Autowired
     private MemberService memberService;
-    private ProductRepository productRepository;
+    private CartService cartService;
 
     @GetMapping("/my/mypage")
     public String myPage(@AuthenticationPrincipal CustomUserDetails customUserDetails, Model model) {
@@ -29,11 +29,11 @@ public class MyPageController {
     }
 
     @GetMapping("/my/cart")
-    public String cartPage(@AuthenticationPrincipal CustomUserDetails customUserDetails, Model model) {
+    public String cartPage(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam("quantity") int quantity, @RequestParam("idx") Long idx, Model model) {
         String userid = customUserDetails.getUsername();
-        CartDTO cartDTO = memberService.findByUserid(userid);
 
-        model.addAttribute("cartDTO", cartDTO);
+        cartService.saveCart(userid, quantity, idx);
+        model.addAttribute("cartProducts", cartService.getCartProducts(userid));*/
         return "/my/cart";
     }
 }
---
+++ src/main/java/lhc/portfolio_test/service/CartService.java
@@ -0,0 +1,10 @@
+package lhc.portfolio_test.service;
+
+import lhc.portfolio_test.dto.CartDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    public CartDTO ()
+}
