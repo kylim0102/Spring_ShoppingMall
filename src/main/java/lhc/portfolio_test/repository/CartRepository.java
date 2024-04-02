2b31880f7a708d371ce62196802e9fe0ad42cc84
kylim0102 <ghckddl0102@naver.com>
2024-04-02 17:44:26 +0900

    20240402

--- src/main/java/lhc/portfolio_test/repository/CartRepository.java
+++ src/main/java/lhc/portfolio_test/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package lhc.portfolio_test.repository;
 
-public class CartRepository {
+import lhc.portfolio_test.entity.CartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository <CartEntity, Long> {
 }
--- src/main/java/lhc/portfolio_test/service/CartService.java
+++ src/main/java/lhc/portfolio_test/service/CartService.java
@@ -1,10 +1,44 @@
 package lhc.portfolio_test.service;
 
 import lhc.portfolio_test.dto.CartDTO;
+import lhc.portfolio_test.entity.CartEntity;
+import lhc.portfolio_test.repository.CartRepository;
+import lhc.portfolio_test.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartService {
 
-    public CartDTO ()
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+
+    public CartService(CartRepository cartRepository, ProductRepository productRepository) {
+        this.cartRepository = cartRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public void saveCart(String username, int quantity, Long idx) {
+        productRepository.findById(idx);
+
+        CartEntity existingCart = cartRepository.findByUserAndProductId(username, idx);
+        if (existingCart != null) {
+            existingCart.updateQuantity(quantity);
+        } else {
+            CartEntity cartEntity = new CartEntity(username, product, quantity);
+            cartRepository.save(cartEntity);
+        }
+    }
+
+    @Transactional
+    public List<CartDTO> getCartProducts(String username) {
+        List<CartEntity> carts = cartRepository.findByUserId(username);
+        return carts.stream()
+                .map(cart -> new CartDTO(cart))
+                .collect(Collectors.toList());
+    }
 }
